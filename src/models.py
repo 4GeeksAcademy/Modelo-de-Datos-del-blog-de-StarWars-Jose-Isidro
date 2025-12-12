c7f926e3e55b65719e4504fd1f8a3a5cf27b8f2b
José Isidro <isidro107109@gmail.com>
2025-12-12 20:30:09 +0000

    falta el diagrama

--- src/models.py
+++ src/models.py
@@ -1,16 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column,relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
     apellido: Mapped[str] = mapped_column(nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     fecha_de_subscripcion: Mapped[int] = mapped_column(nullable=False)
+
 
 class Personaje(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -19,6 +22,7 @@ class Personaje(db.Model):
     altura: Mapped[float] = mapped_column(nullable=False)
     color_de_ojos:  Mapped[str] = mapped_column(nullable=True)
 
+
 class Planeta(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
@@ -26,8 +30,14 @@ class Planeta(db.Model):
     diametro: Mapped[int] = mapped_column(nullable=False)
     terreno: Mapped[str] = mapped_column(nullable=False)
 
+
 class Favorito(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped['User'] = relationship()
-    planeta_id: Mapped['Planeta'] = relationship()
+
+class Personaje_favorito(Favorito):
     personaje_id: Mapped['Personaje'] = relationship()
+
+
+class Planeta_favorito(Favorito):
+    planeta_id: Mapped['Planeta'] = relationship()
